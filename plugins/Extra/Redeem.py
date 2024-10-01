6a2d3a42aa1306668155a075015a117ac5696872
xyzdevoloper <157599470+moviesenderggbot@users.noreply.github.com>
2024-10-01 22:05:51 +0530

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,8 +44,8 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot")],
-                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/IM_JISSHU")]
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="https://t.me/MX_Flix_Robot")],
+                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/xyz_owner_bot")]
             ]
         )
 
