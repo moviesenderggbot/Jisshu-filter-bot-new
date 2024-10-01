a1cfb3b649b5e31754aecedfdb31de9b0c226e2b
xyzdevoloper <157599470+moviesenderggbot@users.noreply.github.com>
2024-10-01 15:34:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,7 +105,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://jisshu-filter-bot-new.onrender.com/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
